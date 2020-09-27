1af05edf5ef87c83df0a3b8431ea61dc059ce5b9
olega <and.olega@gmail.com>
2020-09-27 21:01:49 +0300

    hehe

--- src/lib/detectors/ctdet.py
+++ src/lib/detectors/ctdet.py
@@ -94,4 +94,4 @@ class CtdetDetector(BaseDetector):
       for bbox in results[j]:
         if bbox[4] > self.opt.vis_thresh:
           debugger.add_coco_bbox(bbox[:4], j - 1, bbox[4], img_id='ctdet')
-    debugger.show_all_imgs(pause=self.pause, ip)
+    debugger.show_all_imgs(pause=self.pause)
